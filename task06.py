756882609aa0fd2a682252380623146d1dae0d71
Shravan Gupta <shravangupta72480@gmail.com>
2025-05-10 10:00:03 +0530

    Add files via upload

---
+++ task06.py
@@ -0,0 +1,9 @@
+import math
+num = float(input("Enter a positive number: "))
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)       
+sine_val = math.sin(num)      
+print(f"\nResults for the number {num}:")
+print(f"Square root: {sqrt_val}")
+print(f"Natural logarithm: {log_val}")
+print(f"Sine (in radians): {sine_val}")
